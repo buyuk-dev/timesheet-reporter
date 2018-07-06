d0b755c1f0039f5108893ebf3bbbdb376be627de
Michal Michalski <michal.michalski.ext@nokia.com>
2018-07-06 16:03:49 +0200

    Receipients widget in separate class

--- gui.py
+++ gui.py
@@ -66,12 +66,12 @@ class WorkEntries(tkinter.Frame):
 
         if entries is None:
             for date in common.get_dates_range(config.monday, config.week_length):
-                self.addEntry(date)
+                self.add_entry(date)
         else:
             for date, entry in entries.items():
-                self.addEntry(date, entry["hours"], entry["description"])
+                self.add_entry(date, entry["hours"], entry["description"])
 
-    def addEntry(self, date, hour=8, description=""):
+    def add_entry(self, date, hour=8, description=""):
         print("adding entry: {}".format((date, hour, description)))
 
         app_instance = self.parent.parent
@@ -92,6 +92,30 @@ class WorkEntries(tkinter.Frame):
 
         self.entries.append((label, hour, entry))
 
+        
+class Receipients(tkinter.Frame):
+
+    def __init__(self, parent, receipients=config.receipients, *args, **kwargs):
+        tkinter.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.pack()
+
+        self.recpLabel = tkinter.Label(self, text=config.receipients_section_label)
+        self.recpLabel.pack()
+
+        self.receipientEntries = []
+        for recp in receipients:
+            self.add_receipient(recp)
+
+    def add_receipient(self, receipient=""):
+        app_instance = self.parent.parent
+        var = app_instance.dataModel.addReceipient(receipient)
+        entry = tkinter.Entry(self, width=config.receipient_entry_width, textvariable=var)
+        self.receipientEntries.append(entry)
+        entry.pack()
+        if receipient is not None:
+            var.set(receipient)
+
 
 class GuiApp:
 
@@ -100,15 +124,6 @@ class GuiApp:
         self.tk_context.title(config.app_title)
         self.tk_context.protocol("WM_DELETE_WINDOW", self.tk_context.quit)
         self.onReset()
-
-    def initReceipients(self, receipients=config.receipients):
-        self.receipientsFrame = tkinter.Frame(self.rootFrame)
-        self.recpLabel = tkinter.Label(self.receipientsFrame, text=config.receipients_section_label)
-        self.recpLabel.pack()
-        self.receipientEntries = []
-        self.receipientsFrame.pack()
-        for recp in receipients:
-            self.onNewReceipient(recp)
 
     def reset(self):
         try:
@@ -124,14 +139,6 @@ class GuiApp:
             self.dataModel = model.DataModel()
 
         self.rootFrame.pack()
-
-    def onNewReceipient(self, receipient=None):
-        var = self.dataModel.addReceipient(receipient)
-        entry = tkinter.Entry(self.receipientsFrame, width=config.receipient_entry_width, textvariable=var)
-        self.receipientEntries.append(entry)
-        entry.pack()
-        if receipient is not None:
-            var.set(receipient)
 
     def onGenerate(self):
         entries = self.dataModel.getEntries()
@@ -151,7 +158,7 @@ class GuiApp:
         self.reset()
         data = self.dataModel.load(config.save_path)
         self.menu = AppMenu(self.rootFrame)
-        self.initReceipients(data["receipients"])
+        self.receipients = Receipients(self.rootFrame, data["receipients"])
         self.work_entries = WorkEntries(self.rootFrame, data["entries"])
 
     def onSend(self):
@@ -176,7 +183,7 @@ class GuiApp:
     def onReset(self):
         self.reset()
         self.menu = AppMenu(self.rootFrame)
-        self.initReceipients()
+        self.receipients = Receipients(self.rootFrame)
         self.work_entries = WorkEntries(self.rootFrame)
 
     def run(self):
